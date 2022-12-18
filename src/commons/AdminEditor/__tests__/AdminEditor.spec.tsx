3d121d6f3683bd078b90774cfe9c697f127bbe95
Панфилов Александр <canya.panfilov.95@gmail.com>
2022-12-18 13:43:06 +0300

    тесты для компонента AdminEditor

---
+++ src/commons/AdminEditor/__tests__/AdminEditor.spec.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import AdminEditor from "../AdminEditor";
+
+jest.mock("html-to-draftjs");
+jest.mock("react-draft-wysiwyg", () => {
+  const module = jest.requireActual("react-draft-wysiwyg");
+
+  return {
+    ...module,
+    Editor: ({
+      editorClassName,
+      name,
+    }: {
+      editorClassName: string;
+      name: string;
+    }) => (
+      <>
+        <span>{editorClassName}</span>
+        <span>{name}</span>
+        <span>Editor</span>
+      </>
+    ),
+  };
+});
+
+describe("AdminEditor", () => {
+  let props: {
+    setValue: () => void;
+    errors: { [key: string]: { message: string } };
+    isSubmitted: boolean;
+    trigger: () => void;
+    watch: () => void;
+    isDisabled: boolean;
+    disabledClass: string;
+    name: string;
+    label: string;
+  };
+  beforeEach(() => {
+    props = {
+      setValue: jest.fn,
+      errors: { name: { message: "error message" } },
+      isSubmitted: false,
+      trigger: jest.fn,
+      watch: jest.fn,
+      isDisabled: false,
+      disabledClass: "disabledClass",
+      name: "name",
+      label: "label",
+    };
+  });
+  it("check render component", () => {
+    // @ts-ignore
+    const { getByText } = render(<AdminEditor {...props} />);
+
+    expect(getByText(/label/i)).toBeInTheDocument();
+    expect(getByText(/input-0-2-1 editor-0-2-4/i)).toBeInTheDocument();
+    expect(getByText(/Editor/)).toBeInTheDocument();
+    expect(getByText(/error message/i)).toBeInTheDocument();
+  });
+});
