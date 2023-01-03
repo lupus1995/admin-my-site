515a5c70017483bfac8003288300a37bfe7deda8
Панфилов Александр <canya.panfilov.95@gmail.com>
2023-01-03 03:32:51 +0300

    добавил разделение кода

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import injectSheet from "react-jss";
 import { Routes, Route } from "react-router-dom";
@@ -6,33 +6,92 @@ import { ToastContainer } from "react-toastify";
 
 import { useStylesTag } from "utils/stylesPage";
 
-import {
-  ArticlesForm,
-  ArticleList,
-  Home,
-  MainPage,
-  SignIn,
-  SignUp,
-  Feedback,
-} from "./pages";
 import "react-toastify/dist/ReactToastify.css";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./resetDefaultStylesBrowsers.css";
 import "./roboto.css";
 
+const MainPage = React.lazy(() => import("./pages/Page/MainPage"));
+const SignUp = React.lazy(() => import("./pages/Admin/SignUp"));
+const SignIn = React.lazy(() => import("./pages/Admin/SignIn"));
+const Home = React.lazy(() => import("./pages/Admin/Home"));
+const Feedback = React.lazy(() => import("./pages/Admin/Feedback"));
+const ArticleList = React.lazy(
+  () => import("./pages/Admin/Articles/ArticleList")
+);
+const ArticlesForm = React.lazy(
+  () => import("./pages/Admin/Articles/ArticlesForm")
+);
+
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/admin" element={<Home />} />
-        <Route path="/admin/articles" element={<ArticleList />} />
-        <Route path="/admin/articles/create" element={<ArticlesForm />} />
-        <Route path="/admin/articles/edit/:id" element={<ArticlesForm />} />
-        <Route path="/admin/feedback" element={<Feedback />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <MainPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <SignIn />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin/articles"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <ArticleList />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin/articles/create"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <ArticlesForm />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin/articles/edit/:id"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <ArticlesForm />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin/feedback"
+          element={
+            <Suspense fallback={<p>loading...</p>}>
+              <Feedback />
+            </Suspense>
+          }
+        />
       </Routes>
       <ToastContainer />
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,7 @@ module.exports = {
   },
   output: {
     filename: "[name].js",
+    chunkFilename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
   },
