2ba59277e1f9f97b4cd5b248cd971a71dcd21b16
Панфилов Александр <canya.panfilov.95@gmail.com>
2023-09-29 23:45:43 +0300

    обновил стор

--- store/store.ts
+++ store/store.ts
@@ -2,10 +2,16 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import { auth, authReducer, authMiddleware } from "./services/auth";
 import { tokens, tokensReducer, tokensMiddleware } from "./services/tokens";
+import {
+  authMiddleware as authWebsocketsMiddleware,
+  auth as authWebsocket,
+  authReducer as authWebscoketsReducer,
+} from "../websockets/entities/Auth";
 
 export const rootReducer = combineReducers({
   [auth]: authReducer,
   [tokens]: tokensReducer,
+  [authWebsocket]: authWebscoketsReducer,
 });
 
 export const middlewares = [authMiddleware, tokensMiddleware];
@@ -15,9 +21,11 @@ export const setupStore = () => {
     devTools: true,
     reducer: rootReducer,
     middleware: (getDeafaultMeadlware) => {
-      return getDeafaultMeadlware()
-        .concat(authMiddleware)
-        .concat(tokensMiddleware);
+      return getDeafaultMeadlware().concat(
+        authMiddleware,
+        tokensMiddleware,
+        authWebsocketsMiddleware
+      );
     },
   });
 };
