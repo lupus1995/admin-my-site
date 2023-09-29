2699fb0068a15c2917caf2641405cf3b74793387
Панфилов Александр <canya.panfilov.95@gmail.com>
2023-09-30 01:18:50 +0300

    добавил обертку для стилей

---
+++ websockets/share/WrapperCss/WrapperCss.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+
+const WrapperCss: FC = ({ children }) => <>{children}</>;
+
+export default WrapperCss;
