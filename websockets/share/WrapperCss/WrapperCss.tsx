c54b2bf1dc2fe1a5f20618cfa08cb95e3416a72a
Панфилов Александр <canya.panfilov.95@gmail.com>
2023-11-14 22:42:36 +0300

    правки по WrapperCss, добавил тесты

--- websockets/share/WrapperCss/WrapperCss.tsx
+++ websockets/share/WrapperCss/WrapperCss.tsx
@@ -1,8 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode } from "react";
 
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 
-const WrapperCss: FC = ({ children }) => <>{children}</>;
+const WrapperCss: FC<{ children: ReactNode }> = ({ children }) => (
+  <>{children}</>
+);
 
 export default WrapperCss;
