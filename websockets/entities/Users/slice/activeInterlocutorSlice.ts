5753fd657f2848695fcd33ef1f067353eb46e589
Панфилов Александр <canya.panfilov.95@gmail.com>
2023-12-02 22:04:18 +0300

    добавил слайс для выделения пользователя из чата

---
+++ websockets/entities/Users/slice/activeInterlocutorSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import { RootState } from "store/store";
+
+import { InterlocutorI } from "../types";
+
+const initialState: InterlocutorI = null;
+
+const activeIntelocutorSlice = createSlice({
+  name: "activeInterlocutor",
+  initialState,
+  reducers: {
+    setActiveInterlocutor: (state, action: PayloadAction<InterlocutorI>) => {
+      state = action.payload;
+      return state;
+    },
+  },
+});
+
+export const activeInterlocutorSelector = (state: RootState) =>
+  state.websockets.activeInterlocutor;
+export const { setActiveInterlocutor } = activeIntelocutorSlice.actions;
+export default activeIntelocutorSlice.reducer;
